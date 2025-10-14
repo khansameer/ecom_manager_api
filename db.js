642f42036b539f29be1c31c5009d6df9465bf51e
Sameer <pathansameerahmed@gmail.com>
2025-10-14 16:05:07 +0530

    Initial commit

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 // db.js
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 
 const db = mysql.createPool({
   host: '72.60.29.59',
@@ -7,6 +7,26 @@ const db = mysql.createPool({
   password: 'Mteam^2025',
   database: 'emanager',
   port: 3306
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('❌ Database connection failed:', err.message);
+  } else {
+    console.log('✅ Connected to MySQL database successfully!');
+    connection.release();
+  }
+});
+
+module.exports = db;*/
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.getConnection((err, connection) => {
